abce1dde1d06711e5c897f5e57cc2e9fa842cd09
btseiber <btseiber@oakland.edu>
2018-01-13 15:56:56 -0500

    updates

--- Hardware Development/arduino/test_switch/test_switch.ino
+++ Hardware Development/arduino/test_switch/test_switch.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-const int switchPin = 2;
-const int ledPin = 13;
+
+int pos = 0;  
+int switchPin = 2;
+int ledPin = 13;
 
 void setup() {
 myservo.attach(9); 
@@ -18,22 +18,20 @@ digitalWrite(switchPin, HIGH);
 
 void loop() {
 
-if(digitalRead(switchPin) == LOW){
-
+if(digitalRead(switchPin) == LOW)
+{
 digitalWrite(ledPin, LOW);
-for (pos = 0) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+
+    myservo.write(pos = 0);              // tell servo to go to position in variable 'pos'
     delay(15); 
-}
 }
 else{
 
 digitalWrite(ledPin, HIGH);
-for (pos = 180) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);    
-}
 
+
+  myservo.write(pos = 180);              // tell servo to go to position in variable 'pos'
+  delay(15);    
 }
+//
 }
