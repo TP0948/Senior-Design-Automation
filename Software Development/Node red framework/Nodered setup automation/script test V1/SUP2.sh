b6f2d0ba1df62930d39167c431aba80d8791f699
TP0948 <tjpionk@oakland.edu>
2018-02-01 18:52:25 -0500

    node-red install script v1

---
+++ Software Development/Node red framework/Nodered setup automation/script test V1/SUP2.sh
@@ -0,0 +1,12 @@
+update-nodejs-and-nodered 
+
+update-nodejs-and-nodered 
+
+Npm install Node-red-contrib-bigtimer 
+Npm install Node-red-dashboard 
+Npm install Node-red-contrib-mqtt-broker  
+Npm install Node-red-contrib-camerapi 
+Npm install Node-red-contrib-usbcamera 
+Npm install Node-red-node-mysql	
+
+
