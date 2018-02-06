5054aead8c84cbb3253d9727257523205c66aca5
TP0948 <tjpionk@oakland.edu>
2018-02-06 03:45:13 -0500

    cleanup / documentaion
    Cleanded up files and finished documentaion

--- Software Development/SQL Scripting setup/final - cleaned/lampset/ST1.sh
+++ Software Development/SQL Scripting setup/final - cleaned/lampset/ST1.sh
@@ -1,19 +1,11 @@
 sudo rm -f /etc/apache2apache2.conf
 sudo mkdir -p /etc/apache2/
 sudo cp "/home/pi/Desktop/SQLAMP/apache2.conf" /etc/apache2/apache2.conf
-	
-
 sudo apt-get update
 sudo apt-get upgrade -y 
-
 sudo apt-get install mysql-server python-mysqldb -y 
 sudo apt-get install apache2 -y
 a2enmod rewrite
-
-
-
-
-
 apt-get install php libapache2-mod-php -y
 apt-get install phpmyadmin -y
 
--- Software Development/SQL Scripting setup/final/lampset/ST2.sh
+++
@@ -1,5 +0,0 @@
-
-
-rm -f /etc/mysql/mariadb.cnf
-cp "/home/pi/Desktop/SQLAMP/mariadb.cnf" /etc/mysql/mariadb.cnf
-
